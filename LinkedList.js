7b8cb3418672cb05f4923242e13629271f74e6a2
Amani Magdi Farg <Amani.Magdi@Egypt.TE-Data.core>
2023-10-24 15:11:23 +0300

    test driven development, getByIndex method

--- LinkedList.js
+++ LinkedList.js
@@ -8,6 +8,14 @@ class LinkedList {
     this.head = newNode;
     this.length++;
   }
+  getByIndex(index) {
+    if (index < 0 || index >= this.length) return null;
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
 }
 
 class LinkedListNode {
@@ -17,4 +25,12 @@ class LinkedListNode {
   }
 }
 
-module.exports = LinkedList
+LinkedList.fromValues = function (...values) {
+  const ll = new LinkedList();
+  for (let i = values.length - 1; i >= 0; i--) {
+    ll.insertAtHead(values[i]);
+  }
+  return ll;
+};
+
+module.exports = LinkedList;
--- LinkedList.test.js
+++ LinkedList.test.js
@@ -12,3 +12,30 @@ describe('#insertAtHead', () => {
     expect(ll.length).toBe(2)
   })
 })
+
+describe('#getByIndex', ()=> {
+  describe('with index less than 0', ()=> {
+    test('it will return null', ()=>{
+      const ll = LinkedList.fromValues(10, 20)
+      expect(ll.getByIndex(-1)).toBeNull()
+    })
+  })
+  describe('with index greater than list length', ()=> {
+    test('it will return null', ()=>{
+      const ll = LinkedList.fromValues(10, 20)
+      expect(ll.getByIndex(5)).toBeNull()
+    })
+  })
+  describe('with index 0', ()=> {
+    test('it returns the head', ()=>{
+      const ll = LinkedList.fromValues(10, 20)
+      expect(ll.getByIndex(0).value).toBe(10)
+    })
+  })
+  describe('with index in the middle', ()=> {
+    test('it returns the element at that index', ()=>{
+      const ll = LinkedList.fromValues(10, 20, 30, 40)
+      expect(ll.getByIndex(2).value).toBe(30)
+    })
+  })
+})
